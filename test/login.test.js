b8c4c71dc73bbb372784f861d8942dc6d902f136
benvredenburg <vre19001@byui.edu>
2022-06-02 09:50:27 -0600

    05/28/2022 catchup

--- test/login.test.js
+++ test/login.test.js
@@ -1,11 +1,18 @@
-import {login} from '../utils/login.js';
+import fetch from 'node-fetch';
 
-it ('Should get login token', async () => {
-    const loginRequest = {
-        userName:"vredenburgben@gmail.com",
-        password:"N7T5PpQz!",
-    }
-
-    const token = await login(loginRequest);
-    console.log("Token "+ token)
+it ('Should Respond', async () => {
+    const answer = await fetch ('http://34.168.221.70');
+    console.log(answer);
 })
+
+//import {login} from '../utils/login.js';
+
+// it ('Should get login token', async () => {
+//     const loginRequest = {
+//         userName:"vredenburgben@gmail.com",
+//         password:"N7T5PpQz!",
+//     }
+
+//     const token = await login(loginRequest);
+//     console.log("Token "+ token)
+// })
